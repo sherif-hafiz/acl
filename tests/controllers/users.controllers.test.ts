4d8a85bf85eed182060d21559d50f923236005ea
Sherif Hafiz <sherif.hafiz@kobaltmusic.com>
2018-06-16 19:54:33 +0100

    Creates tests directory

---
+++ tests/controllers/users.controllers.test.ts
@@ -0,0 +1,12 @@
+//import 'mocha';
+import { expect } from 'chai';
+
+describe('User', () => {
+    describe("Simple test", () => {
+        it("This is the a simple test", () => {
+
+
+            expect(3).to.equal(3);
+        });
+    })
+})
