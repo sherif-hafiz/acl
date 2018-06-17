2ff07b9fbc9bf425964120867523b7a987ed23ab
Sherif Hafiz <sherif.hafiz@kobaltmusic.com>
2018-06-17 10:15:03 +0100

    Updates the routes/index.ts to use a new routing structure. Updates new parameter signatures for the user routes.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,19 @@
-//import { Users } from "./users";
+import { Application, Router, Request, Response, NextFunction } from 'express';
+import { UserRouter } from './user.routes';
 
-//export default Users;
+const router = (app: Application) => {
+    //init your main express router
+    const apiRouter: Router = Router();
 
-export * from './user.routes';
+    //handle GET request to /api/v1
+    apiRouter.get('/', (req: Request, res: Response) => {
+        res.status(200).json({ message: "This is where the awesomeness happen..." });
+    });
+
+    app.use('/api/v1', apiRouter);
+    app.use('/user', UserRouter);
+};
+
+export default router;
+
+//export * from './user.routes';
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import { UserController } from "../controllers/user.controllers";
 
 // Assign router to the express.Router() instance
@@ -7,19 +7,24 @@ const router: Router = Router();
 // The / here corresponds to the route that the WelcomeController
 // is mounted on in the server.ts file.
 // In this case it's /welcome
-router.get('/', (req: Request, res: Response) => {
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
     // Reply with a hello world when no name param is provided
     //res.send('Hello, World!');
     UserController.addUser(req, res);
+
 });
 
-router.get('/:id', (req: Request, res: Response) => {
+router.get('/:id', (req: Request, res: Response, next: NextFunction) => {
     // Extract the name from the request parameters
     //let { id } = req.params;
 
     // Greet the given name
     //res.send(`Hello, ${id}`);
     UserController.getUserByID(req, res);
+    next()
+}, (req: Request, res: Response, next: NextFunction) => {
+    console.log("Something could potentially be logged in here");
+    //res.send('Something');
 });
 
 // Export the express.Router() instance to be used by server.ts
